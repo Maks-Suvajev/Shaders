72b229f0d37a7e8b02091b11ccecbc62f2210d38
DESKTOP-58047GN\Maks- <maksimilian.suvajev@gmail.com>
2025-11-20 15:15:19 +0100

    Initial Phong lighting test

--- glsl/fragmentShaderLightTest.fs
+++ glsl/fragmentShaderLightTest.fs
@@ -1,10 +1,36 @@
 #version 330 core
 out vec4 FragColor;
   
+in vec3 Normal;
+in vec3 FragPos;
+
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
 {
-    FragColor = vec4(lightColor * objectColor, 1.0);
+    // Ambient lighting calc
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // Diffuse lighting calc
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    
+    float diff = max(dot(norm, lightDir), 0.0); // if dot < 0 max will return 0.0 instead
+    vec3 diffuse = diff * lightColor;
+
+    // Specular lighting calc
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectrDir = reflect(-lightDir, norm); // Have to negate because its pointing from frag to light currently
+
+    float spec = pow(max( dot(viewDir, reflectrDir), 0.0), 64);
+    vec3 specular = specularStrength * spec * lightColor;
+
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
 }
--- glsl/vertexShaderLightTest.vs
+++ glsl/vertexShaderLightTest.vs
@@ -1,8 +1,11 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
 
 out vec2 TexCoord;
+out vec3 Normal;
+out vec3 FragPos; //Fragment position in world space
 
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
@@ -11,4 +14,6 @@ uniform mat4 projectionMatrix;
 void main()
 {
 	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(aPos, 1.0);
+    FragPos = vec3(modelMatrix * vec4(aPos, 1.0));
+    Normal = aNormal;
 }
